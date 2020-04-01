a75e7766dfe3bebc261465e109cbdea10dd86e1d
kostyanislv <constintinforgit@gmail.com>
2020-04-01 20:32:31 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/app/controller/MainController.java
    #	src/main/java/com/app/service/EquationService.java

--- src/main/java/com/app/controller/MainController.java
+++ src/main/java/com/app/controller/MainController.java
@@ -2,9 +2,10 @@ package com.app.controller;
 
 import com.app.exceptions.BadRequestException;
 import com.app.exceptions.InternalServiceException;
-import com.app.models.EquationBody;
+import com.app.models.Equation;
 import com.app.models.ServiceResponse;
-import com.app.service.EquationService;
+import com.app.services.CacheService;
+import com.app.services.EquationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.Map;
 @RequestMapping(value = "/equation")
 public class MainController {
 
+    CacheService cacheService = new CacheService();
     EquationService equationService = new EquationService();
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -24,47 +26,37 @@ public class MainController {
                                            @RequestParam("sum") Double resultSum,
                                            @RequestParam("rangeFrom") Double min,
                                            @RequestParam("to") Double max) throws InternalServiceException, BadRequestException {
-        EquationBody equation = new EquationBody(firstSlog, resultSum, min, max);
-        equation.verification();
-        logger.info("The calculations were successful");
-        if(this.equationService.find(equation)) {
-            logger.info("Found match in cache");
-            return this.equationService.getValueByKey(equation);
-        }
-        logger.info("Put a new request in the cache");
-        ServiceResponse response = new ServiceResponse();
-        response.setEquationRoot(equation.getEquationRoot());
-        this.equationService.save(equation, response);
-        return response;
+        Equation equation = new Equation(firstSlog, resultSum, min, max);
+        this.equationService.globalVerification(equation);
+        logger.info("Verification was successful");
+        return this.cacheService.getResponse(equation);
     }
 
     @GetMapping(value = "/cache")
-    public Map<EquationBody, ServiceResponse> getCache() {
+    public Map<Equation, ServiceResponse> getCache() {
         logger.info("Get all requests");
-        return this.equationService.getAll();
+        return this.cacheService.getAll();
     }
 
     @DeleteMapping(value = "/cache/delete")
-    public Map<EquationBody, ServiceResponse> deleteEquation(@RequestParam("firstSlog") Double firstSlogan,
+    public void deleteEquation(@RequestParam("firstSlog") Double firstSlogan,
                                @RequestParam("sum") Double sum,
                                @RequestParam("rangeFrom") Double min,
                                @RequestParam("max") Double max) throws BadRequestException, InternalServiceException {
-        EquationBody equation = new EquationBody(firstSlogan, sum, min, max);
-        equation.verification();
-        if(this.equationService.find(equation)) {
-            this.equationService.delete(equation);
+        Equation equation = new Equation(firstSlogan, sum, min, max);
+        this.equationService.globalVerification(equation);
+        if(this.cacheService.find(equation)) {
+            this.cacheService.delete(equation);
             logger.info("Request removed from cache");
         }
         else {
             logger.error("No such request in cache");
         }
-        return this.equationService.getAll();
     }
 
     @DeleteMapping(value = "/cache/deleteAll")
-    public Map<EquationBody, ServiceResponse> deleteAllEquations() {
-        this.equationService.deleteAll();
+    public void deleteAllEquations() {
+        this.cacheService.deleteAll();
         logger.info("The cache is cleared");
-        return this.equationService.getAll();
     }
 }
